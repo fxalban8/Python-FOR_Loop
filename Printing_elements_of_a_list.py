2945a529c15bf23833c95e503cf1a1819ea4fa50
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 22:40:51 -0500

    A program that prints the elements of the list by using the command FOR has been created

---
+++ Printing_elements_of_a_list.py
@@ -0,0 +1,10 @@
+#The FOR command can be used to show the elementos of a list
+
+#let's create a list
+people=["M1","M2","F1","F2","F3"]
+
+
+#let's print each element of the list in one single line each other
+for item in people:
+    print(item)
+    
