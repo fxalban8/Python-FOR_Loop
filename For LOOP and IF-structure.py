5a1f0b9bfa1fde42aabf3d3848c728994ac3a3eb
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 22:46:08 -0500

    Created a program which contains a for loop embedded with an if function and some theory of compilers

---
+++ For LOOP and IF-structure.py
@@ -0,0 +1,5 @@
+children=["B1","G1","B2","B3","G2","G3","G4"]
+for item in children:  #for each element of the list children
+    if "B"in item:  #if theres a B in the element
+        print(item)
+        
